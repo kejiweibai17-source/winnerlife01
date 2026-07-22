c640aee4ccac7b731be70172429da77d44163e78
maxliao2026-design <61307295+bob1127@users.noreply.github.com>
2026-07-22 08:50:12 +0800

    fgdfd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from "next";
 import { Noto_Serif_TC } from "next/font/google";
 import localFont from "next/font/local";
 import { headers } from "next/headers";
+import Script from "next/script";
 import "./globals.css";
 import AppShell from "../../components/AppShell";
 import { getHomeDescription, getHomePageTitle, siteConfig } from "@/lib/site";
+
+const GTM_ID = "GTM-T84JXCM4";
 
 const notoSerifTC = Noto_Serif_TC({
   weight: ["400", "500", "700", "900"],
@@ -45,6 +48,22 @@ export default async function RootLayout({
       <body
         className={`${notoSerifTC.variable} ${geistMono.variable} font-sans antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        <Script id="gtm" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}</Script>
         <AppShell>{children}</AppShell>
       </body>
     </html>
